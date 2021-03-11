790f9437b3f695ece21f84c8997fdf8817d4fa48
SMMainulHasan <mainulhasan7070@gmail.com>
2021-03-12 01:52:56 +0600

    fixed

--- src/components/conditionalImage/ConditionalImage.js
+++ src/components/conditionalImage/ConditionalImage.js
@@ -1,22 +1,17 @@
-import React from 'react';
+import React from "react";
 import Male from "../../images/male.png";
 import Female from "../../images/female.png";
 
 const ConditionalImage = (props) => {
-    const {gender} = props.gender;
-    let displayImage ;
-    (gender ==="Male") ? displayImage = <img src={Male} alt=""/> : displayImage = <img src={Female} alt=""/>
-    
-    // if(props.gender ==="Male"){
-    //     displayImage = <img src={Male} alt=""/>
-    // }
-    return (
-        <div>
-            {
-                displayImage
-            }
-        </div>
-    );
+  let displayImage;
+  props.gender === "Male"
+    ? (displayImage = <img src={Male} alt="" />)
+    : (displayImage = <img src={Female} alt="" />);
+
+  // if(props.gender ==="Male"){
+  //     displayImage = <img src={Male} alt=""/>
+  // }
+  return <div>{displayImage}</div>;
 };
 
 export default ConditionalImage;
